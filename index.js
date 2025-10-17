6df7cef948e51505a8c8ebd8969539617ad90e0c
Drajah223 <davidrajah325@outlook.com>
2025-10-17 16:23:52 +0100

    made a small change.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var http = require("http"); 
-const port = 8000; 
+var http = require("http");
+const port = 8000;
 
-http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World, my name is David. It is nice to meet you!");
-    }).listen(port, function() { 
-        console.log(`Node server is running on port ${port}...`); 
-}); 
+http.createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+        <h1>Hello World, my name is David.</h1>
+        <p><strong>about me:</strong> I am currently studying Computer Science at Goldsmiths University.</p>
+        <p><strong>interests:</strong> I like football, tennis, basketball, and R&B music.</p>
+    `);
+}).listen(port, function() {
+    console.log(`Node server is running on port ${port}...`);
+});
