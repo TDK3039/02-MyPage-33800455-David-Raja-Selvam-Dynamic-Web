97643e678e183747b30b39de4b359bf5d6e0650b
Drajah223 <davidrajah325@outlook.com>
2025-10-21 12:17:57 +0100

    Update index.js - more indepth

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ http.createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`
         <h1>Hello World, my name is David.</h1>
-        <p><strong>about me:</strong> I am currently studying Computer Science at Goldsmiths University.</p>
-        <p><strong>interests:</strong> I like football, tennis, basketball, and R&B music.</p>
+        <p><strong>About me:</strong> I am currently studying Computer Science at Goldsmiths University.</p>
+        <p><strong>Interests:</strong></p>
+        <ul>
+            <li><strong>Football:</strong> My favourite player is Messi — to me, he’s the best footballer in the world.</li>
+            <li><strong>Tennis:</strong> Roger Federer never fails to surprise me. He plays with such elegance that he makes it look effortless.</li>
+            <li><strong>Basketball:</strong> I enjoy the energy and strategy of the game — it keeps me on my toes.</li>
+            <li><strong>Music:</strong> I keep an open mind. I love artists like Mariah Carey, Drake, Giveon, and Michael Jackson. Whatever sounds good to my ears ends up in my playlist.</li>
+            <li><strong>Classical:</strong> I’m currently into Sergei Rachmaninoff’s Piano Concerto No. 2 — especially the last 3 minutes. It feels like paradise on earth.</li>
+        </ul>
     `);
 }).listen(port, function() {
     console.log(`Node server is running on port ${port}...`);
