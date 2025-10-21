0560464cf189e5a3662092f1f46c79b371a4c3dd
Drajah223 <davidrajah325@outlook.com>
2025-10-21 12:29:48 +0100

    updated index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ http.createServer(function(req, res) {
         <p><strong>About me:</strong> I am currently studying Computer Science at Goldsmiths University.</p>
         <p><strong>Interests:</strong></p>
         <ul>
-            <li><strong>Football:</strong> My favourite player is Messi — to me, he’s the best footballer in the world.</li>
+            <li><strong>Football:</strong> My favourite player is Messi — to me, he is the best footballer in the world.</li>
             <li><strong>Tennis:</strong> Roger Federer never fails to surprise me. He plays with such elegance that he makes it look effortless.</li>
-            <li><strong>Basketball:</strong> I enjoy the energy and strategy of the game — it keeps me on my toes.</li>
+            <li><strong>Basketball:</strong> I enjoy the energy and strategy of the game it keeps me on my toes.</li>
             <li><strong>Music:</strong> I keep an open mind. I love artists like Mariah Carey, Drake, Giveon, and Michael Jackson. Whatever sounds good to my ears ends up in my playlist.</li>
-            <li><strong>Classical:</strong> I am currently into Sergei Rachmaninoff’s Piano Concerto No. 2 — especially the last 3 minutes. It feels like paradise on earth.</li>
+            <li><strong>Classical:</strong> I am currently into Sergei Rachmaninoff Piano Concerto No. 2, especially the last 3 minutes. It feels like paradise on earth.</li>
         </ul>
     `);
     //Start server listen on port
